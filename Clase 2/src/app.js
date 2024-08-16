332c9c49481bcdbf86e8f15addba5c75ed8f7266
nicocabrerafit2 <nicocabrera8@outlook.com>
2024-08-15 21:05:12 -0300

    importo y utilizo cookieparser

--- Clase 2/src/app.js
+++ Clase 2/src/app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import handlebars from "express-handlebars";
 import __dirname from "./utils.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = 8080;
 app.engine("handlebars", handlebars.engine());
 app.set("views", __dirname + "/views");
 app.set("view engine", "handlebars");
+app.use(cookieParser("cookieConSecreto"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log("Servidor on http://localhost:" + PORT);
@@ -16,11 +20,20 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 app.get("/getCookie", (req, res) => {
-  res.send(req.Cookies);
-  console.log(req.Cookies);
+  res.send(req.signedCookies);
+  console.log(req.signedCookies);
 });
 app.post("/setCookie", (req, res) => {
+  const { nameClient, mailClient } = req.body;
+  console.log(req.body);
   res
-    .cookie("cookieDePrueba", "Esto es solo una prueba del getCookie")
+    .cookie(nameClient, mailClient, {
+      maxAge: 19999,
+      signed: true,
+    })
     .send("Cookie");
+});
+app.get("/deleteCookie", (req, res) => {
+  res.clearCookie("cookieDePrueba").send("Cookie eliminada");
+  console.log("Cookie eliminada");
 });
