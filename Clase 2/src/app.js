96281462926b5b764b95295a0a7ded77f37b0a14
Nicolas D. Cabrera PC escritorio <nicocabrera8@outlook.com>
2024-08-17 22:48:15 -0300

    agrego boton para eliminar datos de session y hago el contador de visitas

--- Clase 2/src/app.js
+++ Clase 2/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import handlebars from "express-handlebars";
 import __dirname from "./utils.js";
 import cookieParser from "cookie-parser";
+import session from "express-session"
 
 const app = express();
 const PORT = 8080;
@@ -12,6 +13,11 @@ app.use(cookieParser("cookieConSecreto"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname+"/public"))
+app.use(session({
+  secret:"nico",
+  resave:true,
+  saveUninitialized:true
+}))
 
 app.listen(PORT, () => {
   console.log("Servidor on http://localhost:" + PORT);
@@ -40,5 +46,17 @@ app.get("/deleteCookie", (req, res) => {
   res.clearCookie("mail").send("Cookie deleted");
 });
 app.get("/session", (req, res) => {
-  
+  if (!req.session.counter) {
+    req.session.counter = 1
+  }else{
+    req.session.counter++
+  }
+  res.send("Visitas a este sitio: "+req.session.counter)
 });
+app.get("/logout", (req, res) => {
+  req.session.destroy(err =>{
+    if (!err) {res.send("Se cerro la sesion correctamente")
+    }else{
+  res.send({status:"Error al cerrar la sesion",body:err})
+  }
+  })})
