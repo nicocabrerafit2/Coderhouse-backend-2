46acdfd091ddcae982f63260f2ab27fe9ff62687
nicocabrerafit2 <nicocabrera8@outlook.com>
2024-10-22 08:50:47 -0300

    agrego la variable de entorno

---
+++ Segunda entrega/variableDeEntorno.js
@@ -0,0 +1,7 @@
+//Copiar los siguientes datos en un archivo .env a la altura de "package.json"
+PORT = 8080;
+MONGO_STRING =
+  "mongodb+srv://nicocabrera8:Y0BrFdDBQ23amtUR@backendcoderhouse1.nvbxjk0.mongodb.net/?retryWrites=true&w=majority&appName=BackendCoderhouse1";
+
+DB = "EcommerceBackend";
+SECRET = "el secreto";
