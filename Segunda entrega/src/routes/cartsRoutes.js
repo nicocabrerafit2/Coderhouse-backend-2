bfb73c3d87e596c13a98fd41487baa522ee9e707
nicocabrerafit2 <nicocabrera8@outlook.com>
2024-10-22 08:47:28 -0300

    subo las variables de entorno

--- Segunda entrega/src/routes/cartsRoutes.js
+++ Segunda entrega/src/routes/cartsRoutes.js
@@ -11,24 +11,18 @@ export default class CartRouter extends BasicRouter {
     this.post("/:cartId/:productId", ["PUBLIC", "USER"], (req, res) =>
       cartController.addProductInCart(req, res)
     );
-    this.put("/:id", ["PUBLIC", "USER"], (req, res) =>
-      cartController.update(req, res)
-    );
     this.put("/:cartId/:productId", ["PUBLIC", "USER"], (req, res) =>
       cartController.updateQuantityProductInCart(req, res)
-    );
-    this.delete("/:id", ["PUBLIC", "USER"], (req, res) =>
-      cartController.delete(req, res)
     );
     this.delete("/:cartId/:productId", ["PUBLIC", "USER"], (req, res) =>
       cartController.deleteProductFromCart(req, res)
     );
+    this.post("/:cartId/purchase", ["PUBLIC", "USER"], (req, res) =>
+      cartController.addProductInCart(req, res)
+    );
 
-    this.get("*", (req, res) => {
-      res.send("Error no se encontro la ruta");
-    });
-    this.post("*", (req, res) => {
-      res.send("Error no se encontro la ruta");
-    });
+    this.post("/:cartId/purchase", ["PUBLIC", "USER"], (req, res) =>
+      cartController.purchaseCart(req, res)
+    );
   }
 }
